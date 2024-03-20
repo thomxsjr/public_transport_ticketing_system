576ce7354a2e527a6b3964b9fd73d389b0748a4f
Thomas Nirmal <nirmalanchery@gmail.com>
2024-03-20 10:59:12 +0530

    v2.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
 import Header from './components/Header';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   
 
   return (
     <>
-    <h1>Digital Ticketing System For Public Transport</h1>
+    <div className="container">
+      <div className="title">
+        <h1>Digital Ticketing System For Public Transport</h1>   
+      </div>
+      <div className="button">
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+      
+    
     </>
   );
 }
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignUp(){
+
+
+    return(
+        <h1>Sign Up</h1>
+    )
+}
