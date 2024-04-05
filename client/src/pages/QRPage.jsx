f014f80bba06c7e80b56d48d706557d728a3af8a
Thomas Nirmal <nirmalanchery@gmail.com>
2024-04-05 20:49:31 +0530

    v7.5

--- client/src/pages/QRPage.jsx
+++ client/src/pages/QRPage.jsx
@@ -1,11 +1,42 @@
-
+import { Html5QrcodeScanner } from 'html5-qrcode';
+import { useEffect, useState } from 'react';
 
 export default function QRPage(){
 
+    const [scanResult, setScanResult] = useState(null)
+
+    useEffect(()=>{
+
+        const scanner = new Html5QrcodeScanner('reader', {
+            qrbox: {
+                width: 250,
+                height: 250
+            },
+            fps: 5
+        })
+
+        scanner.render(success, error);
+
+        function success(result) {
+            scanner.clear();
+            setScanResult(result)
+        }
+
+        function error(err) {
+            console.warn(err)
+        }
+    },[]);
 
     return(
         <>
-            
+            <div>
+                <h1>QR Code Scanner</h1>
+                {scanResult
+                ?<div>Success: <a href={"https://"+scanResult}>{scanResult}</a></div>
+                :<div id='reader'></div>
+                }
+                
+            </div>
         </>
     )
 }
