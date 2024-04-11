e7181f2e8f9c8ee6ef98037795c525361239852f
Thomas Nirmal <nirmalanchery@gmail.com>
2024-04-11 18:59:12 +0530

    v9.1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../assets/stylesheets/Home.css"
+import bg from "../assets/images/public-transport-bg.jpg"
 
 export default function SignUp(){
 
@@ -7,13 +8,16 @@ export default function SignUp(){
     return(
         <>
             <div className="container">
-            <div className="title">
-                <h1>Digital Ticketing System For Public Transport</h1>   
-            </div>
-            <div className="button">
-                <a href="/signup"><button>Sign Up</button></a>
-                <a href="/signin"><button>Sign In</button></a>
-            </div>
+                <div className="title">
+                    <h1>Digital Ticketing System For Public Transport</h1>   
+                </div>
+                <div>
+                    <img className="mainImg" src={bg} alt="bg" />
+                </div> <br />
+                <div className="signingbutton">
+                    <a href="/signup"><button>Sign Up</button></a>
+                    <a href="/signin"><button>Sign In</button></a>
+                </div>
             </div>
         </>
     )
